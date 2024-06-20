9bef6b3dea71e7373f4230db75def2a6183d4fee
Hanhtunaung97 <littlehanhtunaung97>
2024-06-20 15:00:29 +0800

    action set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrease, increase } from "./store/actions/counterAction";
 
 const App = () => {
+  const state = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const handleAdd = () => {
+    dispatch(increase());
+  };
+  const handleSub = () => {
+    dispatch(decrease());
+  };
   return (
     <div>
-        <div className=" p-20 text-center mt-10">
-        {/* <h1 className=" font-bold text-xl mb-3">{state}</h1> */}
+      <div className=" p-20 text-center mt-10">
+        <h1 className=" font-bold text-xl mb-3">{state}</h1>
         <button
-          // onClick={handleAdd}
+          onClick={handleAdd}
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           Add
         </button>
         <button
-          // onClick={handleSub}
+          onClick={handleSub}
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           Sub
         </button>
-       
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/store/actions/counterAction.js
@@ -0,0 +1,10 @@
+export const increase=() => {
+    return {
+        type:"increase",payload:2
+    }
+}
+export const decrease=() => {
+    return {
+        type:"decrease",payload:2
+    }
+}
